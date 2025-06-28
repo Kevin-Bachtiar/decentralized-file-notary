4ace2b648fa9d1107ba576178fde3b365e6e5d00
Kevin-Bachtiar <kevinmarvellobachtiar@gmail.com>
2025-06-29 01:43:01 +0700

    update

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,11 +4,11 @@ async function main() {
   const [deployer] = await hre.ethers.getSigners();
   console.log("Deploying contract with account:", deployer.address);
 
-  const ContractFactory = await hre.ethers.getContractFactory("Notary"); // Ganti dengan nama kontrak kamu
-  const contract = await ContractFactory.deploy(); // <- deploy dari factory
-  await contract.waitForDeployment(); // << GANTI INI, bukan .deployed()
+  const ContractFactory = await hre.ethers.getContractFactory("Notary");
+  const contract = await ContractFactory.deploy();
+  await contract.waitForDeployment();
 
-  const contractAddress = await contract.getAddress(); // << ambil alamat
+  const contractAddress = await contract.getAddress();
   console.log("✅ Contract deployed to:", contractAddress);
 }
 
